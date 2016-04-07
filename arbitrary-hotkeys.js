4a117531bae240a1d1f6b4c8cb824126aa4219d8
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-04-06 21:49:30 -0300

    Added first capabilities. It works using the example...for now.

--- arbitrary-hotkeys.js
+++ arbitrary-hotkeys.js
@@ -1,1 +1,55 @@
-console.log('hello');
+$(document).ready(function() {
+
+    // Detects all keystrokes on the <div id = "keyStrokes">
+	var keyStrokes = $("#keyStrokes").text().split("\n");
+    //Cleans the HTML
+    $("#keyStrokes").empty();
+
+    var strokesMapping = [];
+    // maps each stroke to an object
+    for(var key in keyStrokes){
+        //Splits on the ||
+        var map = keyStrokes[key].split("||");
+        //Maps to a nice object because it's more organized this way, though not needed.
+        var combination = {
+            key: jQuery.trim(map[0]),
+            targetId: jQuery.trim(map[1])
+        };
+        //Eliminates undefined ones
+        if(combination.key === void 0 || combination.key === '' ||
+                combination.targetId === void 0 || combination.targetId === ''){
+            continue;
+        }
+        //Adds to the avaliable ones.
+        strokesMapping.push(combination);
+    }
+    console.log(strokesMapping);
+
+	var typed = '';
+	var typedDate = new Date();
+		
+	// React on keypress
+	$(window).keypress(function(e) {
+		// If we're in an input, we don't want to interpret the keypresses
+		$('input').keypress(function(e) {
+			e.stopPropagation();
+		});
+		var nowDate = new Date();
+		var diffMs = (nowDate - typedDate);
+		if (diffMs > 1000) {	
+			typed = String.fromCharCode(e.which);
+		} else {
+			typed = typed + String.fromCharCode(e.which);
+		}
+		typedDate = new Date();
+        strokesMapping.some(function (argument) {
+            var index = typed.indexOf(argument.key);
+            if(index >= 0){
+                console.log($('#' + argument.targetId));
+                //Clicks the HTML element. 
+                $('#' + argument.targetId)[0].click();
+            }
+        });
+	});
+
+});
